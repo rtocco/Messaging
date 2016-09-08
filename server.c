9f436993da43881825f4effd7ee2717978337536
Ryan <jtocco@ufl.edu>
2016-09-07 23:44:27 -0400

    c

--- server.c
+++ server.c
@@ -24,11 +24,16 @@ int main(int argc, char **argv) {
     int status;
     int sock;
     int takenSock;
+    char buffer[101];
     socklen_t socketSize;
     struct addrinfo info;
     struct addrinfo *serverInfo;
     struct addrinfo *n;
     struct sockaddr_storage clientAddress;
+
+    struct timeval timeout;
+    timeout.tv_sec = 5;
+    timeout.tv_usec = 0;
 
     memset(&info, 0, sizeof info);
     info.ai_family = AF_UNSPEC;
@@ -47,16 +52,22 @@ int main(int argc, char **argv) {
         printf("Connected\n");
         char *message = "Hello there.\n";
         send(takenSock, message, strlen(message), 0);
+        fd_set set;
+        FD_SET (takenSock, &set);
+        select(FD_SETSIZE, &set, NULL, NULL, &timeout);
+        recv(takenSock, buffer, 100, 0);
+        printf("%s\n", buffer);
         freeaddrinfo(serverInfo);
 
     } else if(!strcmp(option, "-c") && argc == 3) { // run as the client
         status = getaddrinfo(remoteAddress, PORT, &info, &serverInfo);
         sock = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
         connect(sock, serverInfo->ai_addr, serverInfo->ai_addrlen);
-        char buffer[101];
         printf("Connected\n");
         recv(sock, buffer, 100, 0);
         printf("%s\n", buffer);
+        char *message1 = "Hello to you as well!\n";
+        send(sock, message1, strlen(message1), 0);
         freeaddrinfo(serverInfo);
     } else {
         printf("Improper input\n");
