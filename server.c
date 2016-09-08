347f00be1d388767fad7778a5ca04029c8c227c6
Ryan <jtocco@ufl.edu>
2016-09-08 00:59:12 -0400

    e

--- server.c
+++ server.c
@@ -5,6 +5,7 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <sys/time.h>
+#include <unistd.h>
 #include <arpa/inet.h>
 
 #define PORT "9999"
@@ -33,8 +34,12 @@ int main(int argc, char **argv) {
     struct sockaddr_storage clientAddress;
 
     struct timeval timeout;
-    timeout.tv_sec = 5;
+    timeout.tv_sec = 10;
     timeout.tv_usec = 0;
+    fd_set set;
+    FD_ZERO (&set);
+    FD_SET (takenSock, &set);
+    FD_SET (0, &set);
 
     memset(&info, 0, sizeof info);
     info.ai_family = AF_UNSPEC;
@@ -50,26 +55,26 @@ int main(int argc, char **argv) {
         socketSize = sizeof clientAddress;
         takenSock = accept(sock, (struct sockaddr *)&clientAddress, &socketSize);
 
-        printf("Connected\n");
-        char *message = "Hello there.\n";
-        send(takenSock, message, strlen(message), 0);
-        fd_set set;
-        FD_SET (takenSock, &set);
-        FD_SET (0, &set);
-        select(FD_SETSIZE, &set, NULL, NULL, &timeout);
-        recv(takenSock, buffer, 100, 0);
-        printf("%s\n", buffer);
+        //send(takenSock, message, strlen(message), 0)
+        while(1) {
+            status = select(FD_SETSIZE, &set, NULL, NULL, &timeout);
+            if(status != 1) { exit(1); }
+            recv(takenSock, buffer, 100, 0);
+            printf("%s\n", buffer);
+        }
         freeaddrinfo(serverInfo);
 
     } else if(!strcmp(option, "-c") && argc == 3) { // run as the client
         status = getaddrinfo(remoteAddress, PORT, &info, &serverInfo);
         sock = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
         connect(sock, serverInfo->ai_addr, serverInfo->ai_addrlen);
-        printf("Connected\n");
-        recv(sock, buffer, 100, 0);
-        printf("%s\n", buffer);
-        char *message1 = "Hello to you as well!\n";
-        send(sock, message1, strlen(message1), 0);
+
+        while(1) {
+            status = select(FD_SETSIZE, &set, NULL, NULL, &timeout);
+            if(status != 1) { exit(1); }
+            recv(takenSock, buffer, 100, 0);
+            printf("%s\n", buffer);
+        }
         freeaddrinfo(serverInfo);
     } else {
         printf("Improper input\n");
